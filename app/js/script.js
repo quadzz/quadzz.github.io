e8e61c9e76f5b47eeea4bead54861f77f8ccc07a
Karolina Ochlik <karolina.ochlik00@gmail.com>
2023-02-14 08:24:41 +0100

    Layout v2; added data

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log("HELLO!")
+function renderUnderscore() {
+  var underscore = document.getElementById("underscore");
+  var visible = true;
+  window.setInterval(function () {
+    if (visible === true) {
+      underscore.className = "underscore hidden";
+      visible = false;
+    }
+    else {
+      underscore.className = "underscore";
+      visible = true;
+    }
+  }, 400);
+}
+
+renderUnderscore();
